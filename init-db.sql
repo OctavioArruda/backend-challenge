565ddfafe7b5380b8e642bc3f3025ad6b0ea6c38
Octavio Arruda <octavio.amarante@gmail.com>
2024-02-13 14:49:49 -0300

    feat: adds docker-compose, postgres db image integration with setup db script working

---
+++ init-db.sql
@@ -0,0 +1,3 @@
+CREATE TABLE clientes (id BIGINT, nome VARCHAR(255), limite BIGINT);
+
+INSERT INTO clientes (id, nome, limite) VALUES (1, 'Rhys', 100000), (2, 'Ayn', 80000), (3, 'Lyle', 1000000), (4, 'Sari', 10000000), (5, 'Sean', 500000);
